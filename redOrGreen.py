425e3cc2b7d582c58fa4f974f201ec4c7e53b163
AlexeyZh <waltafunk@gmail.com>
2023-09-21 23:48:38 +0300

    Добавлен код для скрапинга и вывода текста из элементов <span> с классом 'green'
    Этот коммит включает в себя следующие изменения:
    - Добавлен код для отправки GET-запроса к веб-странице 'https://www.pythonscraping.com/pages/page1.html'.
    - Использован BeautifulSoup для анализа HTML-кода страницы.
    - Найдены все элементы <span> с классом 'green' и выведен их текстовый контент в консоль.
    
    Этот код позволяет извлекать и отображать текст из определенных элементов на веб-странице.

---
+++ redOrGreen.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('https://www.pythonscraping.com/pages/page1.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+
+nameList = bs.find_all('span', {'class': 'green'})
+for name in nameList:
+    print(name.get_text())
